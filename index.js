59c7c5a75402e6e828981da3ab6361d7827f343d
AZIZBEKJURAEV <ajuraev98@mail.ru>
2022-12-13 09:23:44 +0500

    solved 3 problems

--- index.js
+++ index.js
@@ -4,4 +4,29 @@
 
 const date = new Date();
 const time = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
-console.log(time);
+const weekDay = date.toLocaleString("default", { weekday: "long" });
+const output = `Today is : ${weekDay}. Current time is : ${time}`;
+// console.log(time);
+console.log(output);
+
+//2
+
+function NumReverser(n) {
+  let r = n.toString().split("").reverse().join("");
+  return parseInt(r);
+}
+
+console.log(NumReverser(32243));
+console.log(NumReverser(45522));
+
+//3
+
+for (let i = 0; i <= 15; i++) {
+  if (i === 0) {
+    console.log(`${i} is even`);
+  } else if (i % 2 === 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is odd`);
+  }
+}
